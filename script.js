88786db6a15311910c66753fdabd148f0dc7794b
Ashish Vishwakarma <111213579+ashishvish8738@users.noreply.github.com>
2023-11-24 22:14:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function modifyArray(arr, callback) {
   // do something to arr here
   arr.push(100);
   // then execute the callback function that was passed
-  callback();function getItems(fruitList, ...args, favoriteFruit) {
+  callback();function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...args, favoriteFruit) {
 }
 function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...args, favoriteFruit) {
   return [...fruitList, ...args, favoriteFruit]
