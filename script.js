146dbf8b3e5a9798d54c2ba6cac6fee4f2898537
Ashish Vishwakarma <111213579+ashishvish8738@users.noreply.github.com>
2023-11-24 22:14:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ getItems(["banana", "apple"], "pear", "orange")
 function getItems(fruitList, ...args, favoriteFruit) {
   return [...fruitList, ...args, favoriteFruit]
 }
+function getItems(fruitList, ...args, favoriteFruit) {
+  function getItems(fruitList, ...args, favoriteFruit) {
 getItems(["banana", "apple"], "pear", "orange")
 function getItems(fruitList, ...args, favoriteFruit) {
   return [...fruitList, ...args, favoriteFruit]
