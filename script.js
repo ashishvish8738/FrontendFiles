ca270a2e240340187ba85e6e9c7af5fefd1c0199
Ashish Vishwakarma <111213579+ashishvish8738@users.noreply.github.com>
2023-11-27 10:24:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ function modifyArray(arr, callback) {
   // then execute the callback function that was passed
   callback();function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...args, favoriteFruit) {
 }
+                                                                
+                                                                   function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...arfunction modifyArray(arr, callback) {
+  // do something to arr here
+  arr.push(100);
+  // then execute the callback function that was passedfunction modifyArray(arr, callback) {
+  // do something to arr here
+  arr.push(100);
+  // then execute the callback function that was passed
+  callback();function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...args, favoriteFruit) {
+}function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...arfunction modifyArray(arr, callback) {
+  // do something to arr here
+  arr.push(100);
+  // then execute the callback function that was passedfunction modifyArray(arr, callback) {
+  // do something to arr here
+  arr.push(100);
+  // then execute the callback function that was passed
+  callback();function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...args, favoriteFruit) {
+}
 function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...arfunction modifyArray(arr, callback) {
   // do something to arr here
   function getItems(fruitList, ...args, favoriteFruit) {function getItems(fruitList, ...arfunction modifyArray(arr, callback) {
