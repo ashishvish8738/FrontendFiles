93d2936f10af0f3a597273d11ae42798e2bde64d
Ashish Vishwakarma <111213579+ashishvish8738@users.noreply.github.com>
2023-12-15 11:39:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function modifyArray(arr, callback) {function modifyArray(arr, callback) {
   function modifyArray(arr, callback) {function modifyArray(arr, callback) {function modifyArray(arr, callback) {function modifyArray(arr, callback) {
-  function modifyArray(arr, callback) {function modifyArray(arr, callback) {
+  function modifyArray(arr, callback) {function modifyArray(arr, callback) {function modifyArray(arr, callback) {function modifyArray(arr, callback) {
     function modifyArray(arr, callback) {function modifyArray(arr, callback) {
       function modifyArray(arr, callback) {function modifyArray(arr, callback) {
   function modifyArray(arr, callback) {function modifyArray(arr, callback) {
